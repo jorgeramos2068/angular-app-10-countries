ed294e3fa81f921827ab14a0993786864426411b
Jorge Ramos <jose.ramos@nextiva.com>
2021-09-06 10:47:20 -0500

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { CountriesModule } from './countries/countries.module';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule, CountriesModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, CountriesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
