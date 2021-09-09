35809ecd4b3779c85f7dcea76ddd80ffcbceb33c
Jorge Ramos <jose.ramos@nextiva.com>
2021-09-09 17:03:47 -0500

    Set debounceTime operator in input component

--- src/app/countries/components/country-input/country-input.component.ts
+++ src/app/countries/components/country-input/country-input.component.ts
@@ -1,14 +1,29 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-country-input',
   templateUrl: './country-input.component.html',
 })
-export class CountryInputComponent {
+export class CountryInputComponent implements OnInit {
   @Output() onEnter: EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+  debouncer: Subject<string> = new Subject();
+
   public term: string = '';
+
+  ngOnInit(): void {
+    this.debouncer.pipe(debounceTime(300)).subscribe((value) => {
+      this.onDebounce.emit(value);
+    });
+  }
 
   search(): void {
     this.onEnter.emit(this.term);
+  }
+
+  localKeyPressed() {
+    this.debouncer.next(this.term);
   }
 }
