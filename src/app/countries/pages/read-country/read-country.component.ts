7a1dd740121275469905cdd1c18e0ebab5f1f39f
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 18:17:03 -0600

    Create component to read a country

--- src/app/countries/pages/read-country/read-country.component.ts
+++ src/app/countries/pages/read-country/read-country.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/countries.interface';
 
 @Component({
   selector: 'app-read-country',
   templateUrl: './read-country.component.html',
-  styles: [
-  ]
 })
 export class ReadCountryComponent implements OnInit {
+  public country!: Country;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countriesService: CountriesService
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap((params) =>
+          this.countriesService.getCountryByAlpha(params.id)
+        ),
+        tap(console.log)
+      )
+      .subscribe({
+        next: (country) => {
+          this.country = country;
+        },
+      });
   }
-
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -20,4 +20,9 @@ export class CountriesService {
     const url = `${this.urlApi}/capital/${term}`;
     return this.http.get<Country[]>(url);
   }
+
+  getCountryByAlpha(id: string): Observable<Country> {
+    const url = `${this.urlApi}/alpha/${id}`;
+    return this.http.get<Country>(url);
+  }
 }
